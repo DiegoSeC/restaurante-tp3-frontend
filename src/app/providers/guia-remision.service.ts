5ca1bab5cef428d5e1708c88f952dce0fb285885
LeoAki <leoaki19@gmail.com>
2017-11-17 06:57:18 -0500

    avance 1/2 guia de remision

--- src/app/providers/guia-remision.service.ts
+++ src/app/providers/guia-remision.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { ApiService } from './api.service';
+import { GuiaRemision } from '../models/guia-remision.model';
 
 @Injectable()
 export class GuiaRemisionService {
@@ -9,5 +10,25 @@ export class GuiaRemisionService {
 
     getAll() {
         return this.api.get(this.endpoint);
+    }
+
+    save(object: GuiaRemision) {
+        return this.api.post(this.endpoint, object);
+    }
+
+    update(object: GuiaRemision) {
+        return this.api.put(this.endpoint, object);
+    }
+
+    cancel(id: String) {
+        return this.api.put(this.endpoint, {
+            uuid: id,
+            status: 'Anulado'
+        });
+    }
+
+    getOne(id: String) {
+        const endpoint2 = 'solicitud-cotizacion.json';
+        return this.api.get(endpoint2);
     }
 }
