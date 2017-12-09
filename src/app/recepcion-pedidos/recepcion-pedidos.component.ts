b5dc4fcc09e32e6194216fda784e232eeb571773
Diego Seminario Capulian <diegoasc@gmail.com>
2017-12-08 21:23:13 -0500

    Cronograma

--- src/app/recepcion-pedidos/recepcion-pedidos.component.ts
+++ src/app/recepcion-pedidos/recepcion-pedidos.component.ts
@@ -1,10 +1,13 @@
-import { Component, OnInit } from '@angular/core';
-import { Router, ActivatedRoute } from '@angular/router';
-import { NgbModal, NgbModalRef } from '@ng-bootstrap/ng-bootstrap';
+import { Component, OnInit } from "@angular/core";
+import { Router, ActivatedRoute } from "@angular/router";
+import { NgbModal, NgbModalRef } from "@ng-bootstrap/ng-bootstrap";
+import { NotaPedidoService } from "../providers/nota-pedido.service";
+import { NotaPedido } from "../models/nota-pedido.model";
+import { CookieService } from "ngx-cookie-service";
 
 @Component({
-  selector: 'app-recepcion-pedidos',
-  templateUrl: 'recepcion-pedidos.component.html'
+  selector: "app-recepcion-pedidos",
+  templateUrl: "recepcion-pedidos.component.html"
 })
 export class RecepcionPedidosComponent {
   private modalNotaRef: NgbModalRef;
@@ -13,9 +16,39 @@ export class RecepcionPedidosComponent {
   public checked: boolean;
   public model: string;
 
-  constructor(private modalService: NgbModal) {}
+  public notaPedidos: NotaPedido[];
+  public userWarehouse;
 
-  openNotaPedido(content) {
-    this.modalNotaRef = this.modalService.open(content);
+  public notaDetail: NotaPedido;
+  public userName: string;
+
+  constructor(
+    private modalService: NgbModal,
+    private notaServicio: NotaPedidoService,
+    private cookieService: CookieService
+  ) {
+    this.userWarehouse = JSON.parse(this.cookieService.get("warehouse"));
+    this.userName = this.cookieService.get('me');
+
+    this.getNotaPedidos();
+  }
+
+  getNotaPedidos() {
+    this.notaServicio.getNotaPedidos().subscribe(data => {
+      this.notaPedidos = data["data"].filter(n => {
+        return (
+          n.status === "completed" &&
+          n.warehouse.uuid === this.userWarehouse.uuid
+        );
+      });
+    });
+  }
+
+  openNotaPedido(content, nota: NotaPedido) {
+    this.notaServicio.getNotaPedido(nota.uuid).subscribe(data => {
+      this.notaDetail = data['data'];
+      this.modalNotaRef = this.modalService.open(content);
+    });
+    
   }
 }
